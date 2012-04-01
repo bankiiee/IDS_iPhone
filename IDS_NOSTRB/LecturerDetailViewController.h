a10b8177f913191fc267f41e3556f0b803f15067
Sakarn Limnitikarn <bankiiee@gmail.com>
2012-04-02 03:45:02 +0700

    Done lecturer view & detail
    enhance the app pref

---
+++ IDS_NOSTRB/LecturerDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  LecturerDetailViewController.h
+//  IDS_NOSTRB
+//
+//  Created by Sakarn Limnitikarn on 4/2/55 BE.
+//  Copyright (c) 2555 bankiiee@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LecturerDetailViewController : UIViewController
+
+@end
